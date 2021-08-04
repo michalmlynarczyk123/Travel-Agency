1066e72cf9df0c4a30186d219e600f5ab4e0ecf1
Michał Młynarczyk <michalmlyn@gmail.com>
2021-08-04 20:23:56 +0200

    Datapicker fixed

--- src/components/features/OrderOption/OrderOptionDate.js
+++ src/components/features/OrderOption/OrderOptionDate.js
@@ -1,6 +1,6 @@
 import React, { useState } from 'react';
 import DatePicker from 'react-datepicker';
-import 'react-datepicker/dist/react-datepicker-cssmodules.css';
+import 'react-datepicker/dist/react-datepicker.css';
 import PropTypes from 'prop-types';
 import styles from './OrderOption.module.scss';
 
